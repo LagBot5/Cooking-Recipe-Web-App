2d0fbc01696b2831131ceaab4cd69141f1abfb22
LagBot5 <danefrancis1211@gmail.com>
2024-12-16 09:54:57 -0500

    s

---
+++ recipe-web/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Vegetables from "../components/Vegetables";
+import Popular from "../components/Popular";
+
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
