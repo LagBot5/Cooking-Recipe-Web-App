bf32c1195770fa941ac142cdce818a988e2c10f2
LagBot5 <danefrancis1211@gmail.com>
2025-01-05 20:54:25 -0500

    my version

--- recipe-web/src/App.js
+++ recipe-web/src/App.js
@@ -1,16 +1,16 @@
-// npm install
-// npm install react-scripts
-// npm install framer-motion react-icons react-routers-dom styled-components
-// npm install react-splide
-
-import './App.css';
-import Pages from './pages/Pages';
+import Pages from './pages/Pages'
+import Category from './components/Category';
+import {BrowserRouter} from 'react-router-dom';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <h1>Food Wars</h1>
-      <Pages />
+      <BrowserRouter>
+      <Search />
+      <Category />
+     <Pages />
+     </BrowserRouter>
     </div>
   );
 }
--- recipe-web/src/pages/Home.jsx
+++ recipe-web/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
 
-import React from "react";
+import React from 'react'
 
 function Home() {
   return (
     <div>
-        <Veggie />
-        <Popular />
+        <Veggie/>
+        <Popular/>
     </div>
   )
 }
